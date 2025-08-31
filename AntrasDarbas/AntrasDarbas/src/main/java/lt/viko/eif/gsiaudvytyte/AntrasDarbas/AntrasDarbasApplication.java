2e4927bfa9fc6abd3e93696c137e4a3ddfc94abc
GabrieleSiaud <168407721+GabrieleSiaud@users.noreply.github.com>
2025-08-31 16:06:36 +0300

    Add files via upload

---
+++ AntrasDarbas/AntrasDarbas/src/main/java/lt/viko/eif/gsiaudvytyte/AntrasDarbas/AntrasDarbasApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.gsiaudvytyte.AntrasDarbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntrasDarbasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntrasDarbasApplication.class, args);
+	}
+
+}
